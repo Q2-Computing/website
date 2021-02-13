f401f67d320188afd30c5f1b5af64d0eb9521c61
Q2-Computing <justincadams1987@gmail.com>
2021-02-13 13:18:00 -0500

    Resetting node package management and angular services.

--- Q2-Computing/src/app/app.module.ts
+++ Q2-Computing/src/app/app.module.ts
@@ -1,48 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { VisionComponent } from './vision/vision.component';
-import { SoftwareComponent } from './software/software.component';
-import { HardwareComponent } from './hardware/hardware.component';
-import { TeamComponent } from './team/team.component';
-import { ContactComponent } from './contact/contact.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
-import { BoardAdminComponent } from './board-admin/board-admin.component';
-import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
-import { BoardUserComponent } from './board-user/board-user.component';
-
-import { authInterceptorProviders } from './_helpers/auth.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    VisionComponent,
-    SoftwareComponent,
-    HardwareComponent,
-    TeamComponent,
-    ContactComponent,
-    RegisterComponent,
-    LoginComponent,
-    ProfileComponent,
-    BoardAdminComponent,
-    BoardModeratorComponent,
-    BoardUserComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    BrowserModule
   ],
-  providers: [authInterceptorProviders],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
